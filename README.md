8867566d7aaa678bf301f86ec40c32cd170ac102
shanmugam bhaskaran <shanmugam.bhaskaran@oup.com>
2022-02-22 07:39:09 +0000

    initial version

---
+++ README.md
@@ -0,0 +1,38 @@
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Thu Oct 21 2021 13:39:14 GMT+0000 (Coordinated Universal Time)|
+|**App Generator**<br>@sap/generator-fiori|
+|**App Generator Version**<br>1.3.7|
+|**Generation Platform**<br>SAP Business Application Studio|
+|**Floorplan Used**<br>List Report Object Page V2|
+|**Service Type**<br>SAP System (ABAP On Premise)|
+|**Service URL**<br>http://s4dbasdev130:443/sap/opu/odata/sap/ZOTCARIARENEWAL_SRV/
+|**Module Name**<br>z_otc_mr_sub|
+|**Application Title**<br>Manual Renewal of Subscription|
+|**Namespace**<br>oup.otc|
+|**UI5 Theme**<br>sap_fiori_3|
+|**UI5 Version**<br>1.71.42|
+|**Enable Code Assist Libraries**<br>False|
+|**Add Eslint configuration**<br>False|
+|**Enable Telemetry**<br>True|
+|**Main Entity**<br>ZOTC_C_ARIA_ORDERS|
+|**Navigation Entity**<br>None|
+
+## z_otc_mr_sub
+
+Manual Renewal of Subscription
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  In order to launch the generated app, simply run the following from the generated app root folder:
+
+```
+    npm start
+```
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
---
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,109 @@
+sap.ui.define(
+  ["sap/m/MessageBox", "sap/m/MessageToast"],
+  function (MessageBox, MessageToast) {
+    "use strict";
+
+    let _sIdPrefix;
+    let _sDefferedId = new Date().getTime().toString();
+
+    return {
+      onInit: function () {
+        _sIdPrefix =
+          "oup.otc.zotcmrsub::sap.suite.ui.generic.template.ListReport.view.ListReport::ZOTC_C_ARIA_ORDERS--";
+
+        let oListReport = this.byId(_sIdPrefix + "listReport");
+        oListReport.setShowFullScreenButton(true);
+
+        // deferred group
+        let oDataModel = this.getView()
+          .getController()
+          .getOwnerComponent()
+          .getModel();
+        oDataModel.setDeferredGroups(
+          oDataModel.getDeferredGroups().concat([_sDefferedId])
+        );
+      },
+
+      onAfterRendering: function () {
+        const oContentTable = this.byId(_sIdPrefix + "GridTable");
+        oContentTable.attachBusyStateChanged(this._onBusyStateChanged);
+      },
+
+      _onBusyStateChanged: function (oEvent) {
+        const bBusy = oEvent.getParameter("busy");
+        if (!bBusy) {
+          const oTable = oEvent.getSource();
+          let oTpc = null;
+          // grid table
+          if (sap.ui.table.TablePointerExtension) {
+            oTpc = new sap.ui.table.TablePointerExtension(oTable);
+          } else {
+            oTpc = new sap.ui.table.extensions.Pointer(oTable);
+          }
+          // columns
+          const aColumns = oTable.getColumns();
+          for (let i = aColumns.length; i >= 0; i--) {
+            oTpc.doAutoResizeColumn(i);
+          }
+          aColumns[8].setWidth("300px");
+        }
+      },
+
+      handleRenewSubscription: function (_oEvent) {
+        const oView = this.getView();
+        const oModel = oView.getModel();
+        const oTable = this.byId(_sIdPrefix + "GridTable");
+        const oPlugins = oTable.getPlugins()[0];
+        const aSelectedIndex = oPlugins.getSelectedIndices();
+        let sVbelnPosnr = "";
+        let oContext, oRowData;
+
+        for (let i = 0, iLen = aSelectedIndex.length; i < iLen; i++) {
+          oContext = oTable.getContextByIndex(aSelectedIndex[i]);
+          oRowData = oContext.getObject() || null;
+
+          sVbelnPosnr += `${oRowData.vbeln}-${oRowData.posnr}-${oRowData.ship_to}-${oRowData.trial_rule}|`;
+        }
+
+        // start busy indicator
+        oView.setBusy(true);
+
+        // function import to create PO / contract
+        oModel.callFunction("/RENEW_SUBSCRIPTION", {
+          method: "POST",
+          urlParameters: {
+            vbeln_posnr: sVbelnPosnr,
+          },
+          success: function (oData, _oResponse) {
+            // stop busy indicator
+            oView.setBusy(false);
+
+            // success message
+            MessageToast.show(oData.Message);
+          },
+          error: function (_oError) {
+            try {
+              // stop busy indicator
+              oView.setBusy(false);
+
+              // read error message
+              const sResponseText = _oError.responseText;
+              const oResponse = JSON.parse(sResponseText);
+              const sErrorText = oResponse.error.message.value;
+
+              // size compact
+              const bCompact = !!oView.$().closest(".sapUiSizeCompact").length;
+
+              // error message
+              MessageBox.error(sErrorText, {
+                styleClass: bCompact ? "sapUiSizeCompact" : "",
+              });
+            } catch (error) {
+              // unable to read error message
+            }
+          },
+        });
+      },
+    };
+  }
+);
